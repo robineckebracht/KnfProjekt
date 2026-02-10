34c9e14c24464d0ffc1954fb720c7bd5cf741471
Kylx2128 <72621488+Kylx2128@users.noreply.github.com>
2026-02-10 16:08:30 +0100

    added Sample Formula (a implies b) or (c implies (d and e))

--- src/KnfProjekt/Examples/SampleFormulas.cs
+++ src/KnfProjekt/Examples/SampleFormulas.cs
@@ -5,6 +5,6 @@ namespace KnfProjekt.Examples
     public static class SampleFormulas
     {
         public static Entity Basic =>
-            "a or (b and c)";
+            "(a implies b) or (c implies (d and e))";
     }
 }
